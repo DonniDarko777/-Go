20bd660bac344a303f4dc27bac02e22d0ada166b
Donni Darko <lightrammstein777@mail.ru>
2023-01-26 18:36:26 +0300

    Переменные и типы давнных

---
+++ peremen01.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var massage string // строка
+	massage = "переменная с указание типа данных"
+	var malibu int //число
+	malibu = 7
+	gogo := "переменная без указания типа данных" //без указания типа
+	var nini string                               // нулевая строка в ней ничего не указано выведит пустоту
+	var namber int                                // нулевое сзначение для типи данных число выведет 0
+	var polozh uint                               // выводит положительные тока числа
+	polozh = 100
+	var plav float64 //переменная с плавающей точкой 1.67
+	plav = 99.7654
+	var b bool // истина или лож , булевая херня
+	fmt.Println(b)
+	fmt.Println(plav)
+	fmt.Println(polozh)
+	fmt.Println(namber)
+	fmt.Println(nini)
+	fmt.Println(gogo)
+	fmt.Println(massage)
+	fmt.Println(malibu)
+	fmt.Println(reflect.TypeOf(gogo))    // выводит тип даных feflrct.Typeof(переменная)
+	fmt.Println(reflect.TypeOf(massage)) // выводит тип даных feflrct.Typeof(переменная)
+	fmt.Println(reflect.TypeOf(malibu))  // выводит тип даных feflrct.Typeof(переменная)
+}
